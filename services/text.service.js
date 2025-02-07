6bea5375d1858b7e976e3f988c7fc94c6502e8a0
Inc <121856799+Inc44@users.noreply.github.com>
2025-02-07 10:23:39 +0100

    retab

--- services/text.service.js
+++ services/text.service.js
@@ -51,7 +51,28 @@ const TextService = {
 	{
 		retab(text)
 		{
-			return text;
+			const lines = text.split('\n');
+			const processedLines = [];
+			for (const line of lines)
+			{
+				const match = line.match(/^(\s*)/);
+				if (match)
+				{
+					const whitespace = match[1];
+					const tabMatch = whitespace.match(/^(\t*)/);
+					const existingTabs = tabMatch ? tabMatch[0] : '';
+					const spaceCount = whitespace.length - existingTabs.length;
+					const tabsToAdd = Math.floor(spaceCount / 2);
+					const tabString = '\t'.repeat(tabsToAdd);
+					const trimmedLine = line.replace(/^(\s+)/, '');
+					processedLines.push(existingTabs + tabString + trimmedLine);
+				}
+				else
+				{
+					processedLines.push(line);
+				}
+			}
+			return processedLines.join('\n');
 		},
 		latex(text)
 		{
