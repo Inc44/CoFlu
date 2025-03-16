ff573a88cf1d6ebcc5eaa9717ff747ff2b32554f
Inc <121856799+Inc44@users.noreply.github.com>
2025-03-16 14:38:05 +0100

    search

--- services/ai.service.js
+++ services/ai.service.js
@@ -208,7 +208,7 @@ const AiService = {
 				reqBody.temperature = 0;
 			}
 		}
-		if (model !== 'openrouter' && model !== 'perplexity' && model !== 'sambanova' && model !== 'together' && !modelConfig.reasoning_effort && modelConfig.name !== 'grok-2-1212')
+		if (model !== 'openrouter' && model !== 'perplexity' && model !== 'sambanova' && model !== 'together' && !modelConfig.reasoning_effort && !modelConfig.search_context_size && modelConfig.name !== 'grok-2-1212')
 		{
 			reqBody.max_tokens = modelConfig.max_tokens;
 		}
